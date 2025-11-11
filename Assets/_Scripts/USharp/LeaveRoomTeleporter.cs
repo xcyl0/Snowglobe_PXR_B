d76d3de31391c14b7c92a6848c35686d46d09d6f
Nathan Inbar <nathaninbar@gmail.com>
2025-11-10 18:56:56 -0800

    more crap

---
+++ Assets/_Scripts/USharp/LeaveRoomTeleporter.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LeaveRoomTeleporter : UdonSharpBehaviour
+{
+    [SerializeField] private RoomManager roomManager;
+    [SerializeField] private Transform teleportDestination;
+    public override void Interact()
+    {
+        roomManager.ReleaseLocalRoomAndTeleport(teleportDestination);
+    }
+}
---
+++ Assets/_Scripts/USharp/Teleporter.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleporter : UdonSharpBehaviour
+{
+    [SerializeField] private RoomManager roomManager;
+    //[SerializeField] private Transform destination;
+
+    override public void Interact()
+    {
+        Debug.Log("Interacted with a teleporter");
+        roomManager.AssignAndTeleport();
+        //var localplayer = Networking.LocalPlayer;
+        //localplayer.TeleportTo(destination.position, destination.rotation);
+    }
+}
