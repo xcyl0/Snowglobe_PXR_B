cb50ef9d54c9af555db2a7e75279301c88d8b34c
Nathan Inbar <nathaninbar@gmail.com>
2025-11-12 16:52:24 -0800

    crazy ahh snowglobe

---
+++ Assets/_Scripts/USharp/WallSocketDetector.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WallSocketDetector : UdonSharpBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "fork")
+        {
+            Debug.Log("INSERTING A FORK INTO THIS SOCKET");
+            other.gameObject.SetActive(false);
+            _animator.Play("InsertFork");
+        }
+    }
+}
