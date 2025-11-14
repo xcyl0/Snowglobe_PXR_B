0013c4bef35454b46d22309c9699b0d0ea861f1a
Nathan Inbar <nathaninbar@gmail.com>
2025-11-13 20:42:54 -0800

    constellation anims

---
+++ Assets/_Scripts/USharp/TelescopeController.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TelescopeController : UdonSharpBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private string _fadeInStateName;
+    override public void Interact()
+    {
+        animator.Play(_fadeInStateName);
+    }
+
+}
