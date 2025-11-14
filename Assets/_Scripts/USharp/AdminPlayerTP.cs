cc636b8e1bc13b60c0293d6f8e17d6f986c43bc8
Nathan Inbar <nathaninbar@gmail.com>
2025-11-14 12:13:20 -0800

    admin utilities

---
+++ Assets/_Scripts/USharp/AdminPlayerTP.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using UnityEngine.Apple;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+public class AdminPlayerTP : UdonSharpBehaviour
+{
+    [SerializeField] Transform destination;
+
+    void Start()
+    {
+        if (!Networking.IsInstanceOwner)
+            gameObject.SetActive(false);
+    }
+
+
+    public override void Interact()
+    {
+
+        VRCPlayerApi[] players = new VRCPlayerApi[80];
+        VRCPlayerApi.GetPlayers(players);
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            VRCPlayerApi p = players[i];
+            if (p == null) { continue; }
+            if (p.isInstanceOwner) { continue; }
+
+            SendCustomNetworkEvent(NetworkEventTarget.Others, nameof(RequestTeleport));
+        }
+
+    }
+
+    public void RequestTeleport()
+    {
+        VRCPlayerApi local = Networking.LocalPlayer;
+        if (local == null)
+            return;
+
+        local.TeleportTo(destination.position, destination.rotation);
+    }
+
+
+}
---
+++ Assets/_Scripts/USharp/AdminVideoTrigger.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Video.Components.AVPro;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AdminVideoTrigger : UdonSharpBehaviour
+{
+    [SerializeField] private VRCAVProVideoPlayer player;
+
+    private void Start()
+    {
+        Debug.Log("local player display name: "+ Networking.LocalPlayer.displayName);
+
+        if (!Networking.IsInstanceOwner)
+            gameObject.SetActive(false);
+    }
+
+    public override void Interact()
+    {
+        player.Play();
+    }
+}
