646fb5cdc88b1301c1be4d58a0fa50f1ac94be18
xcyl0 <jacobloat@gmail.com>
2025-11-14 17:19:52 -0500

    Updated Play function

--- Assets/_Scripts/USharp/AdminVideoTrigger.cs
+++ Assets/_Scripts/USharp/AdminVideoTrigger.cs
@@ -1,6 +1,7 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using VRC.SDK3.Components;
 using VRC.SDK3.Video.Components.AVPro;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -8,17 +9,17 @@ using VRC.Udon;
 public class AdminVideoTrigger : UdonSharpBehaviour
 {
     [SerializeField] private VRCAVProVideoPlayer player;
-
+    [SerializeField] private VRCUrl url;
     private void Start()
     {
-        Debug.Log("local player display name: "+ Networking.LocalPlayer.displayName);
+       /* Debug.Log("local player display name: "+ Networking.LocalPlayer.displayName);
 
         if (!Networking.IsInstanceOwner)
-            gameObject.SetActive(false);
+            gameObject.SetActive(false);*/
     }
 
     public override void Interact()
     {
-        player.Play();
+        player.PlayURL(url);
     }
 }
