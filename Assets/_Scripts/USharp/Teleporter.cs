d7873667ecd0cc44520b33de8d8c31b999f83602
Nathan Inbar <nathaninbar@gmail.com>
2025-11-14 15:05:09 -0800

    edited constellation pictures + anims

--- Assets/_Scripts/USharp/Teleporter.cs
+++ Assets/_Scripts/USharp/Teleporter.cs
@@ -1,14 +1,19 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.Events;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class Teleporter : UdonSharpBehaviour
 {
     [SerializeField] private Transform destination;
+    [SerializeField] private UnityEvent optional_event;
+
     public override void Interact()
     {
         Networking.LocalPlayer.TeleportTo(destination.transform.position, Quaternion.identity);
+        if (optional_event != null)
+            optional_event.Invoke();
     }
 }
