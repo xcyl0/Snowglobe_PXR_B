7e0ac61b038f7af4030a8f44e761c6b5fe8d7297
xcyl0 <jacobloat@gmail.com>
2025-11-14 20:07:36 -0500

    Player_AutoTeleporter

---
+++ Assets/_Scripts/USharp/AutoTep_Player.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AutoTep_Player : UdonSharpBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float delay;
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        
+        SendCustomEventDelayedSeconds("TeleportPlayer", delay);
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+    public void TeleportPlayer()
+    {
+        Networking.LocalPlayer.TeleportTo(target.transform.position, Quaternion.identity);
+    }
+}
